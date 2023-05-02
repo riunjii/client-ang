907c33f251a07ba369e97318593bf392cdcf33bb
riunjii <vitor2002_souza@hotmail.com>
2023-05-02 10:35:12 -0300

    test Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clients/clients.component.ts
@@ -0,0 +1,29 @@
+import { ClientService } from './../client.service';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+
+clients: Client[] = [];
+
+constructor (private clientService: ClientService){}
+
+ngOnInit(): void{
+  this.loadClients();
+}
+
+  loadClients(){
+    this.clientService.getClients().subscribe(
+      {
+        next: data => this.clients = data,
+        error: msg => console.log("Erro ao chamar o endpoint") + msg
+      }
+    )
+  }
+
+}
